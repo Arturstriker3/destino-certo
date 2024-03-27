bb183cf5d096d0caf453c5dc142a7a56ab2b1a8b
Arturstriker3 <artur.danielgama@gmail.com>
2024-03-27 09:07:23 -0300

    "Fix": Correção do Gerenciador de Estado Global

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './assets/_main.scss'
 import App from './App.vue'
 import router from './router';
-import { pinia } from './pinia';
+import pinia from './pinia';
 
 const app = createApp(App);
 app.use(router);
