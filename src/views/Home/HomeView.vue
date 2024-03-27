52af4f16c64e30f06e8e51fe3f41f67350a6ea1a
Arturstriker3 <artur.danielgama@gmail.com>
2024-03-27 10:45:47 -0300

    "Feat": Dashboard Home

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,5 +1,57 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="car-sport-outline"></ion-icon></span>
+                        <span class="tittle">Brand Name</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                        <span class="tittle">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="person-add-outline"></ion-icon></span>
+                        <span class="tittle">Customers</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="person-add-outline"></ion-icon></span>
+                        <span class="tittle">Message</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="person-add-outline"></ion-icon></span>
+                        <span class="tittle">Help</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="log-out-outline"></ion-icon></span>
+                        <span class="tittle">Sair</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'HomeView',
+    data() {
+        return {
+            //
+        }
+    }
+}
+</script>
+
+<style lang="scss" src="./style.scss"></style>
