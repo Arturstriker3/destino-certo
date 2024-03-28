410f88762b718f2a8101d595f0384aeb7c9092c9
Arturstriker3 <artur.danielgama@gmail.com>
2024-03-28 11:06:11 -0300

    "Feat": Tabelas para Dashboard

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -39,6 +39,54 @@
                 </div>
             </div>
         </div>
+
+        <div class="details">
+            <div class="recentOrders">
+                <div class="cardHeader">
+                    <h2>Viagens Recentes</h2>
+                    <a href="#" class="btn">Ver Mais</a>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Viagem</td>
+                            <td>Motorista</td>
+                            <td>Placa</td>
+                            <td>Total</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Viagem 1</td>
+                            <td>Gustavo</td>
+                            <td>AHP-6969</td>
+                            <td><span class="status delivered">R$ 2500</span></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="recentCustomers">
+                <div class="cardHeader">
+                    <h2>Motorista Novos</h2>
+                </div>
+                <table>
+                    <tr>
+                        <td width="64px"><div class="imgBx"><img src="/admIMG.jpg" alt=""></div></td>
+                        <td><h4>Alex Brito</h4></td>
+                    </tr>
+                    <tr>
+                        <td width="64px"><div class="imgBx"><img src="/admIMG.jpg" alt=""></div></td>
+                        <td><h4>Marcos Felipe</h4></td>
+                    </tr>
+                    <tr>
+                        <td width="64px"><div class="imgBx"><img src="/admIMG.jpg" alt=""></div></td>
+                        <td><h4>Gustavo Lobo</h4></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
       </template>
     </HomeView>
 </template>
